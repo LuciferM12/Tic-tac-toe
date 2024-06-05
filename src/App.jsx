0a1177ebf9a27cdd6c20cee9131fc89cf7ea4fd9
LuciferM121 <rojasrodriguezmartinomar@gmail.com>
2024-06-05 12:46:07 -0600

    Update board

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react"
 const TURNS = {
   X: 'x',
   O: 'o'
 }
 
-const board = Array(9).fill(null)
+
+const Square = ({children, isSelected, updateBoard, index}) =>{
+  const className = `square ${isSelected ? 'is-selected' : ''}` //Para poder cambiar de quien es el turno
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  return(
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+  
+}
+
+
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) =>{
+    const newBoard = [...board]
+    newBoard[index] = turn //x u o
+    setBoard(newBoard) 
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X 
+    setTurn(newTurn)
+  }
 
   return (
     <main className="board">
@@ -13,15 +39,21 @@ function App() {
         {
           board.map((_, index) => {
             return (
-              <div className="cell" key={index}>
-                <span className="cell__content">
-                  {index}
-                </span>
-              </div>
+              <Square
+                key={index} 
+                index = {index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
             )
           })
         }
 
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   )
